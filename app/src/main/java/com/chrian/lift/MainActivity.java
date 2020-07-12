c54f60e4d020df8a8bfc27901780db94d8665a34
chiran-lm <51204014+chiranlm@users.noreply.github.com>
2020-07-12 15:18:31 +0530

    added customer and driver login

--- app/src/main/java/com/chrian/lift/MainActivity.java
+++ app/src/main/java/com/chrian/lift/MainActivity.java
@@ -2,13 +2,40 @@ package com.chrian.lift;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
+        private Button nDriver, nUser;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+            nDriver = (Button) findViewById(R.id.driver);
+            nUser = (Button) findViewById(R.id.user);
+
+            nDriver.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            });
+
+            nUser.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, UserLoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            });
+        }
+
 }
